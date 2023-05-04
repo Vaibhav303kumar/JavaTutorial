5f12876a414a145d936986c9d474d747eb7a825a
Vaibhav kumar <66459345+Vaibhav303kumar@users.noreply.github.com>
2023-05-04 19:32:27 +0530

    Create Syntax.java

---
+++ Syntax.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");            //output:: Hello world
+  }
+}
+
+
+
+println() -- it will add a new line for each method.
+print() method, which is similar to println().
+The only difference is that it does not insert a new line at the end of the output:
