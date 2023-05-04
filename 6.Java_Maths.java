86243cf276ee4d3ac151a0cd835a4ceee27b0352
Vaibhav kumar <66459345+Vaibhav303kumar@users.noreply.github.com>
2023-05-04 20:16:48 +0530

    Create 6.Java_Maths.java

---
+++ 6.Java_Maths.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main 
+{
+
+    public static void main(String[] args) 
+    {
+      System.out.println(Math.max(5,10));
+      System.out.println(Math.min(5,10));
+      System.out.println(Math.sqrt(100));
+      System.out.println(Math.abs(-10));
+      
+      
+      int randomNum = (int)(Math.random() * 101);  // 0 to 100
+      System.out.println(randomNum);
+    }
+}
+
+
+/*
+
+ 10
+5
+10.0
+10
+73
+
+*/
